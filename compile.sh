576c3d489159492cdb5a0ac866f1a4390ef9838a
zylo <zylo.codes@gmail.com>
2023-10-28 21:05:36 -0500

    preprocessor plaform detection

--- compile.sh
+++ compile.sh
@@ -4,4 +4,7 @@ filename = $1
 
 echo "Compiling..."
 
-echo "cc -std=c99 -Wall ${filename}.c -o ${filename}"
+echo cc -std=c99 -Wall prompt.c -ledit -o prompt
+# echo "cc -std=c99 -Wall ${filename}.c -o ${filename}"
+
+echo "Done"
