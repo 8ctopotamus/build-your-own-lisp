ddb12bcf4d1605b0d75d8cd6d60d591b5611fc3a
zylo <zylo.codes@gmail.com>
2023-10-28 21:02:40 -0500

    wip preprocessor plaform detection

--- prompt.c
+++ prompt.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
-#include <editline/history.h>
+// if Windows
+#ifdef _WIN32
+#include <string.h>
 
 // declare a buffer for user input of size 2048 */
-static char input[2048];
+static char buffer[2048];
+
+// fake readline function
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
+
+// fake add_history function
+void add_history(char* unused) {}
+
+#else
+// Linux or MacOS
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main(int argc, char** argv) {
 	
